0ebe26b02995040081f468956c89c4a2bbaac833
loomou <273226981@qq.com>
2020-09-25 20:38:48 +0800

    侧导航组件

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,31 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
-import StyledNavBar, {StyledMenuItem, MenuIcon} from "./style";
+import StyledNavBar, {StyledMenuItem, MenuIcon, MenuItems} from "./style";
 import Badge from "components/Badge";
+import Avatar from "../Avatar";
+import {faCog, faCommentDots, faEllipsisH, faFolder, faStickyNote, faUsers} from "@fortawesome/free-solid-svg-icons";
+import profileImage from "assets/images/face-male-1.jpg";
+import "styled-components/macro";
 
-function NavBar({children, ...rest}) {
+function NavBar({...rest}) {
   return (
     <StyledNavBar {...rest}>
-      {children}
+      <Avatar src={profileImage} status="online"/>
+      <MenuItems>
+        <MenuItem showBadge active icon={faCommentDots}/>
+        <MenuItem icon={faUsers}/>
+        <MenuItem icon={faFolder}/>
+        <MenuItem icon={faStickyNote}/>
+        <MenuItem icon={faEllipsisH}/>
+        <MenuItem icon={faCog} css={`align-self: end`}/>
+      </MenuItems>
     </StyledNavBar>
   );
 }
 
-function MenuItem({ icon, active, showBadge, ...rest }) {
+function MenuItem({icon, active, showBadge, ...rest}) {
   return (
     <StyledMenuItem active={active} {...rest}>
       <a href="#">
         <Badge show={showBadge}>
-          <MenuIcon active={active} icon={icon} />
+          <MenuIcon active={active} icon={icon}/>
         </Badge>
       </a>
     </StyledMenuItem>
   );
 }
 
-NavBar.propTypes = {
-  children: PropTypes.any
-};
+NavBar.propTypes = {};
 
 export default NavBar;
 
